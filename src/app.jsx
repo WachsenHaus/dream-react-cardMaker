cebb4ea91894f6e0103cd60aa733efa8dc422c0e
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-19 00:41:23 +0900

    <추가>: <postcss 변수기능 연습>

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,19 @@
-import './App.css';
-import React from 'react';  
-import habit from '../../habit-tracker/src/components/habit';
+import "./App.css";
+import React from "react";
 
 function App() {
-  return;
+  return <h1>hello</h1>;
 }
 
 export default App;
+
+//body하단에붙이것
+// <!-- The core Firebase JS SDK is always required and must be listed first -->
+// <script src="/__/firebase/8.2.1/firebase-app.js"></script>
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+//      https://firebase.google.com/docs/web/setup#available-libraries -->
+// <script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
+
+// <!-- Initialize Firebase -->
+// <script src="/__/firebase/init.js"></script>
