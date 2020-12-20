961735efa0edc3bf51e20e2fe623a62abb62ac90
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-20 18:35:20 +0900

    <추가>: <portal 기능 연습>
    💨모달을 사용할 때 주로 사용함

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import React from "react";
+import ReactDOM from "react-dom";
 
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <>
+      <h1>hello</h1>;
+      {ReactDOM.createPortal(
+        <div>
+          <p>안녕하세요</p>
+          <p>실전 리액트 프로그램입니다.</p>
+        </div>,
+        document.getElementById("something")
+      )}
+    </>
+    //createPortal은 모달으 사용할때 사용한다
+  );
 }
 
 export default App;
