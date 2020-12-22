29695ac8aaa0318493e8429d90c8d3caee7960e4
Younghoon CHOI _^ <WachsenHaus@gmail.com>
2020-12-22 19:24:44 +0900

    <추가>: <로그인 유지기능추가함>

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,24 @@
-import "./App.css";
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import styles from "./app.module.css";
 import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
 
 function App({ authService }) {
   return (
-    <>
-      <Login authService={authService}></Login>
-    </>
-    //createPortal은 모달으 사용할때 사용한다
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Login authService={authService} />
+          </Route>
+          <Route path="/maker">
+            <Maker authService={authService} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
-
-//body하단에붙이것
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="/__/firebase/8.2.1/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="/__/firebase/8.2.1/firebase-analytics.js"></script>
-
-// <!-- Initialize Firebase -->
-// <script src="/__/firebase/init.js"></script>
