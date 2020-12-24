abb774efe2298339ee25d49bd53e32ca475ebf6b
Younghoon CHOI _^ <WachsenHaus@gmail.com>
2020-12-24 16:39:51 +0900

    <추가>: <card add form 컴포넌트 추가함> 💨 css, 메소드 추가하였음

---
+++ src/components/card_add_form/card_add_form.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from "react";
+import styles from "./card_add_form.module.css";
+import Button from "../button/button";
+import ImageFileInput from "../image_file_input/image_file_input";
+
+const CardAddForm = ({ onAdd }) => {
+  const formRef = useRef();
+  const nameRef = useRef();
+  const companyRef = useRef();
+  const themeRef = useRef();
+  const titleRef = useRef();
+  const emailRef = useRef();
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const card = {
+      id: Date.now(), //uuid
+      name: nameRef.current.value || "",
+      company: companyRef.current.value || "",
+      theme: themeRef.current.value || "",
+      title: titleRef.current.value || "",
+      email: emailRef.current.value || "",
+      fileName: "",
+      fileUrl: "",
+    };
+    formRef.current.reset();
+    onAdd(card);
+  };
+  return (
+    <form ref={formRef} className={styles.form}>
+      <input
+        ref={nameRef}
+        className={styles.input}
+        type="text"
+        name="name"
+        placeholder="name"
+      />
+      <input
+        ref={companyRef}
+        className={styles.input}
+        type="text"
+        name="company"
+        placeholder="company"
+      />
+      <select ref={themeRef} className={styles.select} name="theme" placeholder="theme">
+        <option placeholder="light">light</option>
+        <option placeholder="dark">dark</option>
+        <option placeholder="colorful">colorful</option>
+      </select>
+      <input
+        ref={titleRef}
+        className={styles.input}
+        type="text"
+        name="title"
+        value="title"
+      />
+      <input
+        ref={emailRef}
+        className={styles.input}
+        type="text"
+        name="email"
+        value="email"
+      />
+      <textarea className={styles.textarea} name="message" value="message " />
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+      </div>
+      <Button name="Add" onClick={onSubmit} />
+    </form>
+  );
+};
+
+export default CardAddForm;
